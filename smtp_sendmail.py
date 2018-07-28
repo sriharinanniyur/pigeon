8fbfef8d0537ad78f563ff0b79669e6c9b070ae8
AaditT <33333433+AaditT@users.noreply.github.com>
2018-07-28 13:09:24 -0700

    Update smtp_sendmail.py

--- smtp_sendmail.py
+++ smtp_sendmail.py
@@ -1,5 +1,11 @@
 import smtplib
 
+# Standard responses to action items
+aff = "Thank you for the reminder. Looking forward to meeting you!"
+neg = "Thanks for the invite, but I will not be able to attend due to a conflict. Would any other times work?"
+med = "I will get back to you as soon as possible."
+
+# Function responsible for sending an email
 def smtp_send(email_address, user_pwd, target, content):
   mail = smtplib.SMTP('smtp.gmail.com', 587)
   mail.ehlo()
@@ -7,3 +13,12 @@ def smtp_send(email_address, user_pwd, target, content):
   mail.login(email_address,user_pwd)
   mail.sendmail(email_address,target, content)
   mail.close()
+
+# Function responsible for easily sends standard response based on user's choice
+def smart_reply(email_address, user_pwd, target, response_type):
+  if response_type == aff:
+    smtp_send(email_address, user_pwd, target, aff)
+  elif response_type == neg:
+    smtp_send(email_address, user_pwd, target, neg)
+  elif response_type == med:
+    smtp_send(email_address, user_pwd, target, med)
