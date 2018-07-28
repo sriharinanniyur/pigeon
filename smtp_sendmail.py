41468bae08e352f62a58a3bb651c35db4bfa77f1
AaditT <33333433+AaditT@users.noreply.github.com>
2018-07-28 12:52:40 -0700

    Update smtp_sendmail.py

--- smtp_sendmail.py
+++ smtp_sendmail.py
@@ -1,15 +1,9 @@
 import smtplib
 
-mail = smtplib.SMTP('smtp.gmail.com', 587)
-mail.ehlo()
-mail.starttls()
-
-email_address = input(print("Gmail address: "))
-user_pwd = input(print("Password: "))
-target = input(print("To: "))
-content = input(print("Content: "))
-
-
-mail.login(email_address,user_pwd)
-mail.sendmail(email_address,target, content)
-mail.close()
+def smtp_send(email_address, user_pwd, target, content):
+  mail = smtplib.SMTP('smtp.gmail.com', 587)
+  mail.ehlo()
+  mail.starttls()
+  mail.login(email_address,user_pwd)
+  mail.sendmail(email_address,target, content)
+  mail.close()
