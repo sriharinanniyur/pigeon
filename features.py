ca2b4893f256e9c74768732c1ebd9225ac0bd995
AaditT <33333433+AaditT@users.noreply.github.com>
2018-07-30 17:50:21 -0700

    Update features.py

--- features.py
+++ features.py
@@ -31,3 +31,21 @@ def sentiment_analysis(sentence):
                 return "neutral: " + str(round(100 * dominant_emotion)) + "%"
             elif (key == "compound"):
                 return "compound: " + str(round(100 * dominant_emotion)) + "%"
+
+
+# Dependencies
+# pip3 install summarizer
+from summarizer import summarize
+
+def sum(title,text,count):
+  summarize(title,text,count)
+
+# Example input (Martin Luther King Jr.'s "I Have A Dream" speech)
+# Output:
+# [
+# 'It is obvious today that America has defaulted on this promissory noteinsofar as her citizens of color are concerned.', 
+# 'We refuse to believe that there are insufficient funds in the great vaults of opportunity of this nation.', 
+# 'This is no time to engage in the luxury of cooling off or to take the tranquilizing drug of gradualism.', 
+# 'I say to you today, my friends, so even though we face the difficulties of today and tomorrow, I still have a dream.', 
+# 'It is a dream deeply rooted in the American dream.'
+# ]        
