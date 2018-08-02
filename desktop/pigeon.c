8df42373da0239b001164e0660245ea833d70150
sriharinanniyur <snanniyur@gmail.com>
2018-08-02 15:59:04 -0700

    Add GUI authentication.

--- desktop/pigeon.c
+++ desktop/pigeon.c
@@ -4,13 +4,16 @@
 #include "mail.h"
 #include "gui.h"
 
+
+GtkWidget *auth_window, *auth_uname_entry, *auth_pwd_entry, *auth_btn, *auth_box;
 void get_uname_pwd(void);
+void authenticate(GtkWidget *, gpointer);
 
 int main(int argc, char **argv)
 {
+    gtk_init(&argc, &argv);
     get_uname_pwd();
     fetch_headers(USERNAME, PASSWORD);
-    gtk_init(&argc, &argv);
     main_gui();
     system("rm HEADERS MESSAGE SENDMESSAGE TEXT");
     return 0;
@@ -18,12 +21,28 @@ int main(int argc, char **argv)
 
 void get_uname_pwd(void)
 {
-    char username[MAXLEN], *password;
-    printf("username: ");
-    fgets(username, MAXLEN, stdin);
-    username[strlen(username)] = '\0';
-    strcpy(USERNAME, username);
-    password = getpass("password: ");
-    strncpy(PASSWORD, password, MAXLEN);
-    free(password);
+    auth_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(auth_window), 300, 150);
+    gtk_window_set_title(GTK_WINDOW(auth_window), "Username and Password");
+
+    auth_uname_entry = gtk_entry_new();
+    auth_pwd_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(auth_pwd_entry), FALSE);
+    auth_btn = gtk_button_new_with_label("Authenticate");
+    auth_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, -1);
+	gtk_box_pack_start(GTK_BOX(auth_box), auth_uname_entry, FALSE, FALSE, 1);
+	gtk_box_pack_start(GTK_BOX(auth_box), auth_pwd_entry, FALSE, FALSE, 1);
+	gtk_box_pack_start(GTK_BOX(auth_box), auth_btn, FALSE, FALSE, 1);
+	gtk_container_add(GTK_CONTAINER(auth_window), auth_box);
+	g_signal_connect(auth_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(auth_btn, "clicked", G_CALLBACK(authenticate), NULL);
+    gtk_widget_show_all(auth_window);
+    gtk_main();
+}
+
+void authenticate(GtkWidget *p, gpointer data)
+{
+    strncpy(USERNAME, gtk_entry_get_text(GTK_ENTRY(auth_uname_entry)), MAXLEN);
+    strncpy(PASSWORD, gtk_entry_get_text(GTK_ENTRY(auth_pwd_entry)), MAXLEN);
+    gtk_main_quit();
 }
