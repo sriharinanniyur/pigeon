bbc4ea25d0b89d937b41318495107d6e22ce6806
AaditT <aadit.trivedi03@gmail.com>
2018-08-15 10:58:46 -0700

    Update osx-install.sh

--- py-analytics/osx-install.sh
+++ py-analytics/osx-install.sh
@@ -1,6 +1,57 @@
 #!/bin/bash
+# requires this "sudo chmod +x osx-install.sh" to be executable through "./osx-install.sh"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew reinstall python3
 brew reinstall gtk3
 brew reinstall sbcl
 pip3 install summarizer nltk wordcloud matplotlib
+
+# Fun animation
+
+echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,@@@"
+sleep 0.2
+echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,,,,,@@@@"
+sleep 0.2
+echo " @@@@@@@@@@@@@@@%((*@@@@@@@@,,,,,,,,@@@@"
+sleep 0.2
+echo " @@@@@@@@@@@@@@(((((@@@@@&.,,,,,,,,,@@@@"
+sleep 0.2
+echo " @@@@@@@@@@@@@((((((/@@*****.,,,,,,,,@@@"
+sleep 0.2
+echo " @@@@@@@@@@@@((((((((*******..,,,,,,,,@@"
+sleep 0.2
+echo " @@@@@@@@@@@%((((((*******,....,,,,,@@@@"
+sleep 0.2
+echo " @@@@@@@@@@@(((((*********......,,,@@@@@"
+sleep 0.2
+echo " @@@@@@@@@@((((((*******.........,,@@@@@"
+sleep 0.2
+echo " @,,,,,@@@(((((((******...........@@@@@@"
+sleep 0.2
+echo " (**%****@(((((((*****   ........@@@@@@@"
+sleep 0.2
+echo " @@#%///////(,********    .....#@@@@@@@@"
+sleep 0.2
+echo " @@@@((((((*************(((( . @@@@@@@@@"
+sleep 0.2
+echo " @@@@@%(((*************/((((@@@@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@(********//********@@@@@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@@*********************.@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@@@***********************...*@@@"
+sleep 0.2
+echo " @@@@@@@@@@@,***********************(((@"
+sleep 0.2
+echo " @@@@@@@,,,,..,.....@@@@@@@@@@@@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@*,/,,,......@@@@@@@@@@@@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@***,*/,,....@@@@@@@@@@@@@@@@@@@@"
+sleep 0.2
+echo " @@@@@@@****,,,,,...@@@@@@@@@@@@@@@@@@@@"
+sleep 1
+
+echo "Pigeon v1.0 Installed!"
