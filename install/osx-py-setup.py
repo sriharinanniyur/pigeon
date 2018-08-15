c9602d3d0ce1bd7b518ed5fbed35623dc5b4c0b2
AaditT <aadit.trivedi03@gmail.com>
2018-08-15 14:38:22 -0700

    Update osx-py-setup.py

--- install/osx-py-setup.py
+++ install/osx-py-setup.py
@@ -6,9 +6,6 @@ import os
 # install brew, the amazing Mac package manager
 os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 
-# make sure python3 is installed
-os.system("brew reinstall python3")
-
 # installs the gui interface
 os.system("brew reinstall gtk3")
 
