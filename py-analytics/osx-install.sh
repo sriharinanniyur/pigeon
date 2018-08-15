8555e8eb2ca087ee0236973103b8cd0a45654c52
AaditT <aadit.trivedi03@gmail.com>
2018-08-15 10:36:59 -0700

    Create osx-install.sh

---
+++ py-analytics/osx-install.sh
@@ -0,0 +1,5 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew reinstall python3
+brew reinstall gtk3
+brew reinstall sbcl
+pip3 install summarizer nltk wordcloud matplotlib
