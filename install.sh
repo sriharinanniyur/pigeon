3d53df5a61da03addf9aa290405b062f52b5a99d
AaditT <aadit.trivedi03@gmail.com>
2018-08-18 16:05:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,74 +1,26 @@
 #!/bin/bash
 
 # written by Aadit Trivedi
-# requires this "sudo chmod +x osx-install.sh" to be executable through "./osx-install.sh"
 
 read -r -p "Would you like to install pigeon? ___ GB will be added. [Y/n] " input
 
 case $input in
     [yY][eE][sS]|[yY])
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew reinstall python3
-  brew reinstall gtk3
-  brew reinstall sbcl
-  pip3 install summarizer nltk wordcloud matplotlib --user
-
-  # Fun animation
-
-  echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,,,,,@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@@@@@%((*@@@@@@@@,,,,,,,,@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@@@@(((((@@@@@&.,,,,,,,,,@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@@@((((((/@@*****.,,,,,,,,@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@@((((((((*******..,,,,,,,,@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@%((((((*******,....,,,,,@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@(((((*********......,,,@@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@((((((*******.........,,@@@@@"
-  sleep 0.2
-  echo " @,,,,,@@@(((((((******...........@@@@@@"
-  sleep 0.2
-  echo " (**%****@(((((((*****   ........@@@@@@@"
-  sleep 0.2
-  echo " @@#%///////(,********    .....#@@@@@@@@"
-  sleep 0.2
-  echo " @@@@((((((*************(((( . @@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@%(((*************/((((@@@@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@(********//********@@@@@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@@*********************.@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@@@***********************...*@@@"
-  sleep 0.2
-  echo " @@@@@@@@@@@,***********************(((@"
-  sleep 0.2
-  echo " @@@@@@@,,,,..,.....@@@@@@@@@@@@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@*,/,,,......@@@@@@@@@@@@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@***,*/,,....@@@@@@@@@@@@@@@@@@@@"
-  sleep 0.2
-  echo " @@@@@@@****,,,,,...@@@@@@@@@@@@@@@@@@@@"
-  sleep 1
-
-  echo "Pigeon v1.0 Installed!"
-
+    	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo "Waking up Python3..."
+	brew reinstall python3
+	echo "Calling gtk3..."
+	brew reinstall gtk3
+	echo "Bringing on sbcl..."
+	brew reinstall sbcl
+	echo "Assembling Python libraries..."
+	pip3 install summarizer nltk wordcloud matplotlib --user
+	echo "Pigeon v1.0 Installed!"
     [nN][oO]|[nN])
 		echo "Okay, please close this window to cancel the installation."
        		;;
-
     *)
 	echo "Invalid input..."
 	exit 1
 	;;
 esac
-
