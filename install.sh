f997dbac344b8dc37b8cc70f4b1289829a2bbf84
AaditT <aadit.trivedi03@gmail.com>
2018-08-18 16:03:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,57 +3,72 @@
 # written by Aadit Trivedi
 # requires this "sudo chmod +x osx-install.sh" to be executable through "./osx-install.sh"
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew reinstall python3
-brew reinstall gtk3
-brew reinstall sbcl
-pip3 install summarizer nltk wordcloud matplotlib --user
+read -r -p "Would you like to install pigeon? ___ GB will be added. [Y/n] " input
 
-# Fun animation
+case $input in
+    [yY][eE][sS]|[yY])
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew reinstall python3
+  brew reinstall gtk3
+  brew reinstall sbcl
+  pip3 install summarizer nltk wordcloud matplotlib --user
 
-echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,@@@"
-sleep 0.2
-echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,,,,,@@@@"
-sleep 0.2
-echo " @@@@@@@@@@@@@@@%((*@@@@@@@@,,,,,,,,@@@@"
-sleep 0.2
-echo " @@@@@@@@@@@@@@(((((@@@@@&.,,,,,,,,,@@@@"
-sleep 0.2
-echo " @@@@@@@@@@@@@((((((/@@*****.,,,,,,,,@@@"
-sleep 0.2
-echo " @@@@@@@@@@@@((((((((*******..,,,,,,,,@@"
-sleep 0.2
-echo " @@@@@@@@@@@%((((((*******,....,,,,,@@@@"
-sleep 0.2
-echo " @@@@@@@@@@@(((((*********......,,,@@@@@"
-sleep 0.2
-echo " @@@@@@@@@@((((((*******.........,,@@@@@"
-sleep 0.2
-echo " @,,,,,@@@(((((((******...........@@@@@@"
-sleep 0.2
-echo " (**%****@(((((((*****   ........@@@@@@@"
-sleep 0.2
-echo " @@#%///////(,********    .....#@@@@@@@@"
-sleep 0.2
-echo " @@@@((((((*************(((( . @@@@@@@@@"
-sleep 0.2
-echo " @@@@@%(((*************/((((@@@@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@(********//********@@@@@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@@*********************.@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@@@***********************...*@@@"
-sleep 0.2
-echo " @@@@@@@@@@@,***********************(((@"
-sleep 0.2
-echo " @@@@@@@,,,,..,.....@@@@@@@@@@@@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@*,/,,,......@@@@@@@@@@@@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@***,*/,,....@@@@@@@@@@@@@@@@@@@@"
-sleep 0.2
-echo " @@@@@@@****,,,,,...@@@@@@@@@@@@@@@@@@@@"
-sleep 1
+  # Fun animation
 
-echo "Pigeon v1.0 Installed!"
+  echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&,@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,,,,,@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@@@@@%((*@@@@@@@@,,,,,,,,@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@@@@(((((@@@@@&.,,,,,,,,,@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@@@((((((/@@*****.,,,,,,,,@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@@((((((((*******..,,,,,,,,@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@%((((((*******,....,,,,,@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@(((((*********......,,,@@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@((((((*******.........,,@@@@@"
+  sleep 0.2
+  echo " @,,,,,@@@(((((((******...........@@@@@@"
+  sleep 0.2
+  echo " (**%****@(((((((*****   ........@@@@@@@"
+  sleep 0.2
+  echo " @@#%///////(,********    .....#@@@@@@@@"
+  sleep 0.2
+  echo " @@@@((((((*************(((( . @@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@%(((*************/((((@@@@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@(********//********@@@@@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@@*********************.@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@@@***********************...*@@@"
+  sleep 0.2
+  echo " @@@@@@@@@@@,***********************(((@"
+  sleep 0.2
+  echo " @@@@@@@,,,,..,.....@@@@@@@@@@@@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@*,/,,,......@@@@@@@@@@@@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@***,*/,,....@@@@@@@@@@@@@@@@@@@@"
+  sleep 0.2
+  echo " @@@@@@@****,,,,,...@@@@@@@@@@@@@@@@@@@@"
+  sleep 1
+
+  echo "Pigeon v1.0 Installed!"
+
+    [nN][oO]|[nN])
+		echo "Okay, please close this window to cancel the installation."
+       		;;
+
+    *)
+	echo "Invalid input..."
+	exit 1
+	;;
+esac
+
